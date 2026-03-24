5b9b5c7c3ca3d88c3d3ae17096e4752550cf0280
allan roberto <allanroberto18@gmail.com>
2026-03-24 07:45:06 -0300

    create project

---
+++ src/main/java/br/com/alr/api/sbkafkaproducersample/SbKafkaProducerSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.alr.api.sbkafkaproducersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaProducerSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbKafkaProducerSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/alr/api/sbkafkaproducersample/SbKafkaProducerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alr.api.sbkafkaproducersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaProducerSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
